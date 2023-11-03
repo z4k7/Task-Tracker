0d2b7b6303971004b9b055a23313831c1af2d2ac
Sachin Kizhakkepurath <sachinkp1997@gmail.com>
2023-11-03 12:05:08 +0530

    first commit

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text:string;
+  @Input() color:string;
+  @Output() btnClick= new EventEmitter()
+
+  constructor(){
+    this.text = '';   
+    this.color = '';
+  }
+
+  ngOnInit(): void {}
+
+  onClick(){
+    this.btnClick.emit()
+    
+  }
+
+}
